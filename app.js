dae040286d40f567b77e13b6ab6dd01c2b169761
Matt <matthewthaskins@gmail.com>
2021-10-11 20:00:11 +0100

    Added cors

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ var logger = require('morgan');
 var app = express();
 
 require('./bin/migrate')();
+const cors = require('cors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
