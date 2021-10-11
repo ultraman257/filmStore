b9cbad738596461590ea5996f68110c5fc8e631c
Matt <matthewthaskins@gmail.com>
2021-10-11 17:42:58 +0100

    Now?

---
+++ readme.md
@@ -0,0 +1,16 @@
+# FileStore Backend
+
+
+This is the API used to power the film store. To run download, install modules and configuire the enviroment.
+
+Environment Variables:
+- rethinkdb - the hostname or ip for rethinkdb
+- dbName - the name of the table used for rethink
+- TMDB_KEY - API key for themoviedb.org (used to power all the content).
+
+Pastable .env
+```env
+rethinkdb=
+dName=
+TMDB_KEY=
+```
---
+++ routes/films.js
@@ -0,0 +1,126 @@
+var express = require('express');
+var router = express.Router();
+
+let axios = require('axios');
+
+let r = require('../bin/database');
+
+// films, get fils
+router.get('/list', function (req, res, next) {
+
+  let filter = {};
+
+  if (req.query.seen && req.query.seen === true) {
+    filter.watched = false;
+  }
+
+  // Get films
+  r.table('films').filter(filter).then(async result => {
+
+    let results = [];
+
+    for (let i = 0; i < result.length; i++) {
+
+      let film = result[i];
+
+      const querys = [
+        `https://api.themoviedb.org/3/movie/${film.dbId}?api_key=${process.env.TMDB_KEY}`,
+        `https://api.themoviedb.org/3/movie/${film.dbId}/watch/providers?api_key=${process.env.TMDB_KEY}`
+      ]
+      let resObj = {};
+      await Promise.all(querys.map(async query => {
+        let response = await axios.get(query, {})
+          .then(r => r.data);
+
+        if (response.results) {
+          response = response.results.GB;
+        }
+
+        resObj = { ...response, ...resObj }
+      }))
+      results.push({ ...resObj, ...film })
+
+    }
+
+    return res.json({ message: "success", films: results });
+
+
+  }).catch(error => {
+    console.log(error);
+  });
+
+
+});
+
+// films, seen film
+router.post('/seen', (req, res) => {
+
+  if(!req.body) return res.json({ message: "failed", reason: "body missing"});
+
+  r.table('films').get(req.body.id).update({ watched: req.body.watched }).then(result => {
+    res.json({ message: "success" });
+  }).catch(error => {
+    return res.json({ message: "failed", reason: "an error occured when writing to the database."});
+  })
+
+})
+
+// films, seen
+
+// films, edit
+
+// files, add
+router.post('/add', (req, res) => {
+
+  let newFilm = {
+    watched: false
+  };
+
+  if(!req.body) return res.json({ message: "failed", reason: "body is missing"});
+
+  if(req.body.movieDb) {
+    newFilm.movieDb = req.body.movieDb;
+    newFilm.dbId = req.body.dbId;
+  } else {
+    newFilm.movieDb = false;
+    newFilm.dbId = null;
+    newFilm.title = req.body.title;
+  }
+
+  r.table('films').insert(newFilm).then(result => {
+
+    return res.json({ message: "success" })
+
+  }).catch(error => {
+    return res.json({ message: "failed", reason: "an error occured when writing to the database."});
+  })
+
+})
+
+// files, dynamic add
+router.get('/search', function(req, res) {
+
+  validateFields(req.query, "film", res);
+
+  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_KEY}&query=${req.query.film}`, {}).then(result => {
+    return {films: result.data, ...result.status}
+  }).then(result => {
+    console.log(result);
+    res.json({ message: "success", query: req.query.filmName,  matches: result.films.results.slice(0, 5)});
+  });
+
+})
+
+module.exports = router;
+
+function validateFields(body, name, res) {
+
+  if(!body.hasOwnProperty(name)) {
+    return res.json({ message: "failed", reason: "missing field " + name })
+  }
+
+  if(!body[`${name}`].length > 0) {
+    return res.json({ message: "failed", reason: "missing field " + name })
+  }
+
+}
